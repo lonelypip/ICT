d8c98f92d1f0e8f79a1567289d0510170f4468d8
Kapa <darik_11.98@mail.ru>
2019-04-09 11:32:34 +0600

    Question model

--- ICTapp/admin.py
+++ ICTapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Information, Specialty, Discipline
+from .models import Information, Specialty, Discipline, Question
 
 
 admin.site.register(Information)
 admin.site.register(Specialty)
 admin.site.register(Discipline)
+
+admin.site.register(Question)
--- ICTapp/context_processors.py
+++ ICTapp/context_processors.py
@@ -1,17 +1,15 @@
 from .forms import QuestionForm
 from .models import Question
 from django.shortcuts import redirect
+from django.contrib import messages
 
 def question_form(request):
+   if request.method == 'POST':
+      bound_form = QuestionForm(request.POST)
+      if bound_form.is_valid():
+         bound_form.save()
+         messages.success(request, f'Ваш вопрос принят. Прошу ожидайте ответа')
    return {'question_form': QuestionForm()}
 
 
-   # full_name = request.POST.get('full_name')
-   # email = bound_form.cleaned_data['email']
-   # phone = bound_form.cleaned_data['phone']
-   # text = bound_form.cleaned_data['text']
-   # print(full_name)
-   # # question = Question.objects.create(post=post, full_name=full_name, text=text)
-   # # comment.save()
-   # # messages.success(request, f'Ваш комментарий в рассмотрений')
-   # return redirect('home_url')
+   
